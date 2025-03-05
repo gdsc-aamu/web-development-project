ac8e7ae9d13d8e87140cd89c35fd564e9af8e307
Thabo Ibrahim Traore <traorethabo39@gmail.com>
2025-03-05 17:07:43 -0600

    Fixed missing <html> and <body> tags in layout.js

--- my-professor-frontend/app/layout.js
+++ my-professor-frontend/app/layout.js
@@ -1,18 +1,17 @@
-// TODO: This file is responsible for setting up the overall layout of the app.
-// 1. Import necessary components (Navbar, Footer).
-// 2. Ensure that all pages wrap around a consistent layout.
-// 3. Use `children` to allow pages to be rendered inside this layout.
-// 4. Import the global CSS styles to apply them throughout the app.
-
 export default function Layout({ children }) {
   return (
-    <div>
-      {/* TODO: Add a Navbar component at the top */}
-      
-      {/* TODO: This is where different pages will be displayed */}
-      <main>{children}</main>
+    <html lang="en">
+      <head>
+        <title>Your Website Title</title>
+      </head>
+      <body>
+        {/* TODO: Add a Navbar component at the top */}
+        
+        {/* TODO: This is where different pages will be displayed */}
+        <main>{children}</main>
 
-      {/* TODO: Add a Footer component at the bottom */}
-    </div>
+        {/* TODO: Add a Footer component at the bottom */}
+      </body>
+    </html>
   );
 }
